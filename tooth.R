ab001eb7004b214043229e80e53384d4ae31fdec
unknown <k.papastamos@MW70AJSA47709L.dir.svc.accenture.com>
2015-09-24 13:06:49 +0300

    improved the plotting in tooth.R

--- tooth.R
+++ tooth.R
@@ -1,5 +1,6 @@
 data(ToothGrowth)
-library(ggplot2)
+require(ggplot2)
+require(gridExtra)
 
 data=ToothGrowth
 
@@ -7,4 +8,16 @@ data$dose=as.factor(data$dose)
 
 suppbydose = interaction(data$supp,data$dose)
 
-pl = ggplot(data,aes(y=len, x=suppbydose))+geom_boxplot()
+p1 = ggplot(data,aes(y=len, x=supp, fill=supp))+geom_boxplot()+ guides(fill=guide_legend(title="Supplement"))
+p1 = p1 + labs(x = "Supplement",y = "Length")
+
+p2 = ggplot(data,aes(y=len, x=dose, fill=dose))+geom_boxplot()+ guides(fill=guide_legend(title="Dose"))
+p2 = p2 + labs(x = "Dose",y = "Length")
+
+p3 = ggplot(data,aes(y=len, x=suppbydose, fill=suppbydose))+geom_boxplot()+ guides(fill=guide_legend(title="Supplement by Dose"))
+p3 = p3 + labs(x = "Supplement by Dose",y = "Length")
+
+grid.arrange(p1, p2, p3, ncol = 2)
+
+
+
